4f9ac328d7810cccd5d6f49ac6152c8202dca5a7
jackdisalvatore <jackp.disalvatore@gmail.com>
2022-02-07 13:23:11 -0600

    contract to add and remove liquidity from uniswap v2

--- README.md
+++ README.md
@@ -5,8 +5,9 @@ from: https://www.youtube.com/playlist?list=PLO5VPQH6OWdX-Rh7RonjZhOd9pb9zOnHW
 .env
 
 ```sh
-WE3_INFURA_PROJECT_ID="d0ec29a0058f463ca96ee64c20d1a256"
-DAI_WHALE="0x2fEb1512183545f48f6b9C5b4EbfCaF49CfCa6F3"
+WE3_INFURA_PROJECT_ID=d0ec29a0058f463ca96ee64c20d1a256
+DAI_WHALE=0x2fEb1512183545f48f6b9C5b4EbfCaF49CfCa6F3
+WETH_WHALE=0xEab23c1E3776fAd145e2E3dc56bcf739f6e0a393
 ```
 
 ### Test
@@ -17,12 +18,14 @@ source .env
 # using infura.io
 ganache-cli \
 --fork https://mainnet.infura.io/v3/$WE3_INFURA_PROJECT_ID \
---unlock $DAI_WHALE \
+--unlock $DAI_WHALE --unlock $WETH_WHALE \
 --networkId 999
 ```
 
 In another terminal
 
 ```bash
-truffle test --network mainnet_fork test/test-uniswapv2.js
+# truffle test --network mainnet_fork test/test-uniswapv2.js
+
+env $(cat .env) npx truffle test --network mainnet_fork test/test-uniswapv2-liquidity.js
 ```
--- test/test-uniswapv2-liquidity.js
+++ test/test-uniswapv2-liquidity.js
@@ -1,19 +1,75 @@
-// const BN = require("bn.js");
-// const { sendEther, pow } = require("./util");
-// const { WETH, DAI, WETH_WHALE, DAI_WHALE } = require("./config");
+const BN = require("bn.js");
+const { sendEther, pow } = require("./util");
+const { WETH, DAI, WETH_WHALE, DAI_WHALE } = require("./config");
 
 const IERC20 = artifacts.require("IERC20");
 const TestUniswapV2Liquidity = artifacts.require("TestUniswapV2Liquidity");
 
 contract("TestUniswapV2Liquidity", (accounts) => {
     const CALLER = accounts[0];
-
+    const TOKEN_A = WETH;
+    const TOKEN_A_WHALE = WETH_WHALE;
+    const TOKEN_B = DAI;
+    const TOKEN_B_WHALE = DAI_WHALE;
+    const TOKEN_A_AMOUNT = pow(10, 18);
+    const TOKEN_B_AMOUNT = pow(10, 18);
 
     let contract;
     let tokenA;
     let tokenB;
 
     beforeEach(async() => {
-        // tokenA = await IERC20.at(TOKEN_A);
-    })
+        tokenA = await IERC20.at(TOKEN_A);
+        tokenB = await IERC20.at(TOKEN_B);
+
+        contract = await TestUniswapV2Liquidity.new();
+
+        // send ETH to cover tx fee
+        await sendEther(web3, accounts[0], TOKEN_A_WHALE, 1);
+        await sendEther(web3, accounts[0], TOKEN_B_WHALE, 1);
+
+        // console.log("WETH_WHALE (1): ", process.env.WETH_WHALE);
+        // console.log("WETH_WHALE: ", WETH_WHALE);
+        // console.log("TOKEN_A_WHALE: ", TOKEN_A_WHALE);
+
+        await tokenA.transfer(CALLER, TOKEN_A_AMOUNT, { from: TOKEN_A_WHALE });
+        await tokenB.transfer(CALLER, TOKEN_B_AMOUNT, { from: TOKEN_B_WHALE });
+
+        await tokenA.approve(contract.address, TOKEN_A_AMOUNT, { from: CALLER });
+        await tokenB.approve(contract.address, TOKEN_B_AMOUNT, { from: CALLER });
+    });
+
+    it("add liquidity and remove liquidity", async () => {
+        let tx = await contract.addLiquidity(
+            tokenA.address,
+            tokenB.address,
+            TOKEN_A_AMOUNT,
+            TOKEN_B_AMOUNT,
+            {
+                from: CALLER,
+            }
+        );
+
+        console.log("==== add liquidity ====");
+        for (const log of tx.logs) {
+            console.log(
+                `${log.args.message} ${log.args.val}`
+            );
+        }
+
+        tx = await contract.removeLiquidity(
+            tokenA.address,
+            tokenB.address,
+            {
+                from: CALLER
+            }
+        );
+
+        console.log("==== removeLiquidity ====");
+        for (const log of tx.logs) {
+            console.log(
+                `${log.args.message} ${log.args.val}`
+            );
+        }
+    });
 });
